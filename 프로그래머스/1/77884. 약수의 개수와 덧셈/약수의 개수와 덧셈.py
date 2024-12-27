58e99556e8b45e66fafe62ce40863c02657ba335
euuns <108703924+euuns@users.noreply.github.com>
2024-12-27 10:37:08 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 21.63 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.py
@@ -1,19 +1,15 @@
 def solution(left, right):
     result = 0
-    num = [x for x in range(left, right+1)]
-    su = []
 
     for i in range(left, right+1):
-        n = []
+        cnt = 0
         for j in range(1, i+1):
             if i%j == 0:
-                n.append(j)
-        su.append(len(n))
-
-    for i in range(len(su)):
-        if su[i]%2 == 0:
-            result += num[i]
+                cnt += 1
+        
+        if cnt%2 == 0:
+            result += j
         else:
-            result -= num[i]
-    
+            result -= j
+
     return result
